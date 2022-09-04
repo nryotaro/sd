d0a2f9fabec9f2edcf2ab5cce087bab8de9c1a5d
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2022-09-04 23:54:37 +0900

    airbnb

--- systems/systems/airbnb/leetcode.py
+++ systems/systems/airbnb/leetcode.py
@@ -1,6 +1,6 @@
 """Airbnb System Design."""
 import sys
-from hornet.digraph import Digraph, SubGraph
+from hornet.digraph import Digraph, SubGraph, Cluster
 from systems.nodes import (
     Kafka,
     Elasticsearch,
@@ -28,36 +28,38 @@ if __name__ == "__main__":
         cleanup=False,
     ):
         internet = Internet("Internet")
-        with SubGraph({"rank": "same"}):
-            frontend = NextJs("Frontend")
-            api_gateway = Spring("API Gateway")
-            frontend >> api_gateway
-        internet >> HAProxy("Load Balancer") >> frontend
+        with Cluster({"label": "doge"}):
+            with SubGraph({"rank": "same"}):
+                frontend = NextJs("Frontend")
+                api_gateway = Spring("API Gateway")
+                frontend >> api_gateway
 
-        internet >> api_gateway
-        api_gateway >> Spring("User") >> PostgreSQL("DB")
-        hotel = Spring("Hotel")
-        api_gateway >> hotel >> PostgreSQL("DB")
-        booking = Spring("Booking")
-        api_gateway >> booking
-        (
-            api_gateway
-            >> Spring("Hotel")
-            >> [
-                PostgreSQL("DB"),
-                Redis("Cache"),
-                Elasticsearch("Search"),
-            ]
-        )
+            load_balancer = HAProxy("Load Balancer")
+            load_balancer >> frontend
+            api_gateway >> Spring("User") >> PostgreSQL("DB")
+            internet >> [load_balancer, api_gateway]
+            hotel = Spring("Hotel")
+            api_gateway >> hotel >> PostgreSQL("DB")
+            booking = Spring("Booking")
+            api_gateway >> booking
+            (
+                api_gateway
+                >> Spring("Hotel")
+                >> [
+                    PostgreSQL("DB"),
+                    Redis("Cache"),
+                    Elasticsearch("Search"),
+                ]
+            )
 
-        booking >> [PostgreSQL("DB")]
+            booking >> [PostgreSQL("DB")]
 
-        messages = Kafka("User activities")
-        analysis = Python("Analysis")
+            messages = Kafka("User activities")
+            analysis = Python("Analysis")
 
-        (
-            [hotel, booking]
-            >> messages
-            << analysis
-            >> Cassandra("User activities")
-        )
+            (
+                [hotel, booking]
+                >> messages
+                << analysis
+                >> Cassandra("User activities")
+            )
