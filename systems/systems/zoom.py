3f9ff4a9b5a02399646cc0ce474f88ffad7f2b9e
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2022-11-29 14:32:12 -0500

    use stix2

--- systems/systems/zoom.py
+++ systems/systems/zoom.py
@@ -2,16 +2,9 @@
 import sys
 from hornet.digraph import Digraph, Cluster
 from systems.nodes import (
-    Kafka,
     NodeJs,
-    S3,
-    Elasticsearch,
-    Cassandra,
-    CloudFront,
-    NextJs,
     Redis,
     Spring,
-    PostgreSQL,
     User,
 )
 
